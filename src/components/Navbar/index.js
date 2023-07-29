18b7109f29630d7604a52e38131d3c66835fdf4a
Neelpanji <35188935+Neelpanji@users.noreply.github.com>
2023-07-29 11:30:48 +0530

    Update index.js
    only PodLeaf

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -71,7 +71,7 @@ const Navbar = ({ open, toggle}) => {
                     <NavbarContainer>
                         <NavLogo  to={process.env.PUBLIC_URL+"/#hero"} onClick={() => closeSidebar(open)} scroll={el => scrollWithOffset(el)}>
                         {/* <img src={baseUrl+'assets/images/logo.png'} className='nav-logo' alt="ABC Power" /> */}
-                        <div>PodLeaF Productions</div>
+                        <div>PodLeaF</div>
                         </NavLogo>
                         {/* <SocialMedia>
                             <a href="#"><SocialMediaIcon src="/assets/images/logos/instagram.png"/></a>
